9ce4c1a48221b2cce3d2b62aaee0a51c80f1eedf
omar-slf <nurullayevumarali1@gmail.com>
2021-11-28 12:50:14 +0500

    firebase chnage

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBJqtm0rTqWltmf889NKdnH1TNf9IdqPH0",
+  authDomain: "whether-app-from-omar.firebaseapp.com",
+  projectId: "whether-app-from-omar",
+  storageBucket: "whether-app-from-omar.appspot.com",
+  messagingSenderId: "1095974445046",
+  appId: "1:1095974445046:web:f6e1ff408def827baa71d7"
+};
+
+const app = initializeApp(firebaseConfig);
